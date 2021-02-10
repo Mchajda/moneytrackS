6eb7fb052b6d6d5df89eaca2d2d8b370b90cb852
Maciej <48365963+Mchajda@users.noreply.github.com>
2021-02-10 22:04:34 +0100

    inflacja entity

---
+++ src/Entity/Inflation.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InflationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=InflationRepository::class)
+ */
+class Inflation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    public function setYear(string $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
